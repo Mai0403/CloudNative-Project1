74c03601291247ba92829972496e65fe99dd59c6
Srey Mai Phou <105780579+Mai0403@users.noreply.github.com>
2025-01-31 17:48:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.delete('/delete/:email', (req, res) => {
 // app.listen(PORT, () => {
 //     console.log(`Server running on http://localhost:${PORT}`);
 // });
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
